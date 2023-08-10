72bbaf91eae5e3ad8852b614be766f3d84ba0d6b
Taeho <rjsdnxogh55@gmail.com>
2023-08-11 02:09:21 +0900

    #3 feat: Implement RssReader & Add Exceptions
    - 구독 등록 서비스에 사용할 RssReader를 구현했습니다.
    - 이에 따라 예외를 추가했습니다.

--- ppap/src/main/java/com/ppap/ppap/_core/exception/BaseExceptionStatus.java
+++ ppap/src/main/java/com/ppap/ppap/_core/exception/BaseExceptionStatus.java
@@ -20,8 +20,17 @@ public enum BaseExceptionStatus {
     // AccessToken, RefreshToken
     REFRESH_TOKEN_INVALID("유효하지 않은 토큰입니다.", 400),
     REFRESH_TOKEN_EXPIRED("만료된 토큰입니다.", 400),
-    REFRESH_TOKEN_NOT_FOUND("존재하지 않는 토큰입니다.", 404);
+    REFRESH_TOKEN_NOT_FOUND("존재하지 않는 토큰입니다.", 404),
 
+    // RSS link
+    RSS_LINK_INVALID("유효하지 않은 RSS 링크입니다.", 400),
+    RSS_LINK_NOT_PNU("부산대 RSS 링크가 아닙니다.", 400),
+    RSS_LINK_UNKNOWN_ERROR("RSS 링크 검증 중 알 수 없는 에러가 발생했습니다. 운영자에게 문의해주세요.", 500),
+    RSS_LINK_NETWORK_ERROR("RSS 링크 검증 중 네트워크 에러가 발생했습니다. 잠시 후에 다시 시도해주세요.", 502),
+    RSS_NOTICE_LINK_MISMATCH("RSS와 공지사항 링크 학과가 일치하지 않습니다.", 400),
+
+    // subscribe
+    SUBSCRIBE_ALREADY_EXIST("이미 존재하는 구독입니다.", 400);
 
     @Getter
     private final String message;
