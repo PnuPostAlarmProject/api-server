c8496e6f1bcfa9ca4a3519bb23cdfe6a5dd72f3f
Taeho <rjsdnxogh55@gmail.com>
2023-08-30 15:39:21 +0900

    #19 fix: Fix GetSubscribeContentUserCase Error
    - NoticeId로 검색해야하는 내용을 SubscribeId로 검색하는 버그를 수정했습니다.

--- ppap/src/main/java/com/ppap/ppap/application/usecase/GetSubscribeContentUseCase.java
+++ ppap/src/main/java/com/ppap/ppap/application/usecase/GetSubscribeContentUseCase.java
@@ -1,11 +1,10 @@
 package com.ppap.ppap.application.usecase;
 
 import com.ppap.ppap._core.exception.BaseExceptionStatus;
-import com.ppap.ppap._core.exception.Exception403;
 import com.ppap.ppap._core.exception.Exception404;
 import com.ppap.ppap.domain.scrap.dto.ScrapFindByContentIdDto;
 import com.ppap.ppap.domain.scrap.service.ScrapReadService;
-import com.ppap.ppap.domain.subscribe.dto.SubscribeWithContentDto;
+import com.ppap.ppap.domain.subscribe.dto.SubscribeWithContentScrapDto;
 import com.ppap.ppap.domain.subscribe.entity.Content;
 import com.ppap.ppap.domain.subscribe.entity.Subscribe;
 import com.ppap.ppap.domain.subscribe.service.ContentReadService;
@@ -28,22 +27,22 @@ public class GetSubscribeContentUseCase {
     /**
      * 컨텐츠 화면을 켰을 때 줄 서비스 로직
      */
-    public SubscribeWithContentDto execute(Optional<Long> subscribeId, User user, Pageable pageable) {
+    public SubscribeWithContentScrapDto execute(Optional<Long> subscribeId, User user, Pageable pageable) {
         // 등록중인 subscribes
-        List<Subscribe> subscribeList = subscribeReadService.getSubscribeEntityJoinNotice(user);
+        List<Subscribe> subscribeList = subscribeReadService.getSubscribeEntityList(user);
         subscribeList.sort(Comparator.comparing(Subscribe::getId));
 
         // 현재 클릭 중인 subscribeId
         Long curSubscribeId = subscribeId.orElse(subscribeList.get(0).getId());
 
         // 현재 클릭한 subscribe
-//        Subscribe curSubcribe = subscribeList.stream()
-//                .filter(subscribe -> subscribe.getId().equals(curSubscribeId))
-//                .findFirst().orElseThrow(() -> new Exception404(BaseExceptionStatus.SUBSCRIBE_NOT_FOUND));
+        Subscribe curSubcribe = subscribeList.stream()
+                .filter(subscribe -> subscribe.getId().equals(curSubscribeId))
+                .findFirst().orElseThrow(() -> new Exception404(BaseExceptionStatus.SUBSCRIBE_NOT_FOUND));
 
         // 현재 클릭한 구독의 컨텐츠들
         // One To Many sql 1번 vs notice 1번, content 1번 -> 총 2번 (이거 선택)
-        List<Content> contentList = contentReadService.findByNoticeId(curSubscribeId, pageable);
+        List<Content> contentList = contentReadService.findByNoticeId(curSubcribe.getNotice().getId(), pageable);
 
         // 유저, contentIds에 해당하는 스크랩 가져오기
         List<ScrapFindByContentIdDto> scrapList = scrapReadService.findByContentIds(user.getId(),
@@ -53,6 +52,6 @@ public class GetSubscribeContentUseCase {
                 .map(ScrapFindByContentIdDto::contentId)
                 .collect(Collectors.toSet());
 
-        return SubscribeWithContentDto.of(subscribeList, curSubscribeId, contentList, scrapContentIdSet);
+        return SubscribeWithContentScrapDto.of(subscribeList, curSubscribeId, contentList, scrapContentIdSet);
     }
 }
