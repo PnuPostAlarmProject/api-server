a34b479349650a93eabfb711f321b46e850853e5
Train0303 <rjsdnxogh55@gmail.com>
2024-01-10 23:31:22 +0900

    #25 feat: Implement Fcm Basic Code

--- ppap/src/main/java/com/ppap/ppap/_core/firebase/message/FcmService.java
+++ ppap/src/main/java/com/ppap/ppap/_core/firebase/message/FcmService.java
@@ -2,6 +2,7 @@ package com.ppap.ppap._core.firebase.message;
 
 import com.google.api.core.ApiFuture;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import com.ppap.ppap._core.rss.RssData;
@@ -13,10 +14,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 @Slf4j
 @Service
@@ -28,6 +31,32 @@ public class FcmService {
                                     Set<Subscribe> subscribeSet,
                                     Map<Long, List<Device>> userDeviceGroup) {
         // FCM 푸시 메세지 보내기.
+        Notification notification = Notification.builder()
+            .setTitle("새로운 공지사항이 등록되었습니다.")
+            .build();
+
+        List<Message> messageList = new ArrayList<>();
+        for (List<Device> devices : userDeviceGroup.values()) {
+            devices.forEach((device) -> {
+                    messageList.add(Message.builder()
+                        .setNotification(notification)
+                        .setToken(device.getFcmToken())
+                        .putData("click_action", "NOTIFICATION_CLICK")
+                        // .putData("deeplink", "APP_URL")
+                        .build());
+                });
+        }
+
+        List<List<Message>> chunkMessages = devideMessageList(messageList, 500);
+
+        // 아래를 Reactive 프로그래밍으로 변경해보기
+        try {
+            for (List<Message> messages : chunkMessages) {
+                firebaseMessaging.sendEach(messages);
+            }
+        } catch (FirebaseMessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String sendNotification() {
@@ -47,5 +76,11 @@ public class FcmService {
             else log.error("전송 실패");
         }, Executors.newSingleThreadExecutor());
         return "ok";
+    }
+
+    private List<List<Message>> devideMessageList(List<Message> messageList, int size) {
+        return IntStream.iterate(0, i -> i < messageList.size(), i -> i + size)
+            .mapToObj(i -> messageList.subList(i, Math.min(i + size, messageList.size())))
+            .toList();
     }
 }
