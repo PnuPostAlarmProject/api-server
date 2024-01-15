4dd2f02248fb98de69cb0413c3d1bdd676176dd6
Taeho Kim <39511892+Train0303@users.noreply.github.com>
2024-01-15 21:22:05 +0900

    #25 Implement FCM Service (#30)
    * #25 feat: Implement Fcm Basic Code
    
    * #25 Comment: Add Comment In RssSchedulerService
    
    - 실제 30개를 저장하지만 주석에선 50개라 적혀있는 오류를 수정했습니다.
    - 리팩토링이 필요한 메소드에 주석을 추가했습니다.
    
    * #25 Feat: Implement FcmMessage By CompletableFuture
    
    - FCM 메시지 전송에 대해서 비동기 프로그래밍으로 변경했습니다.
    - FCM Config중 Timeout이 있지만 일단 주석처리를 진행했습니다.
    
    * #25 Feat: Implement Fcm Service
    
    - 갱신된 공지사항마다 FCM 메시지를 보내는 코드를 작성했습니다.

--- ppap/src/main/java/com/ppap/ppap/_core/firebase/message/FcmService.java
+++ ppap/src/main/java/com/ppap/ppap/_core/firebase/message/FcmService.java
@@ -1,9 +1,12 @@
 package com.ppap.ppap._core.firebase.message;
 
 import com.google.api.core.ApiFuture;
+import com.google.firebase.messaging.BatchResponse;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.SendResponse;
 import com.ppap.ppap._core.rss.RssData;
 import com.ppap.ppap.domain.subscribe.entity.Notice;
 import com.ppap.ppap.domain.subscribe.entity.Subscribe;
@@ -11,23 +14,74 @@ import com.ppap.ppap.domain.user.entity.Device;
 import io.netty.util.concurrent.CompleteFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ThreadFactory;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class FcmService {
     private final FirebaseMessaging firebaseMessaging;
+    private final ForkJoinPool forkJoinPool;
+    private static final int CHUNK_SIZE = 500;
 
     public void sendRssNotification(Map<Notice, List<RssData>> filterNoticeRssGroup,
                                     Set<Subscribe> subscribeSet,
                                     Map<Long, List<Device>> userDeviceGroup) {
-        // FCM 푸시 메세지 보내기.
+
+        List<List<Message>> chunkMessages = getChunkMessages(filterNoticeRssGroup, subscribeSet, userDeviceGroup);
+
+        if (userDeviceGroup.keySet().isEmpty())
+            return ;
+
+        ExecutorService executor = Executors.newFixedThreadPool(Math.min(chunkMessages.size(), 10),
+            r -> {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                return t;
+            });
+
+        // 아래를 비동기(CompletableFuture) 프로그래밍으로 변경해보기
+        List<CompletableFuture<BatchResponse>> futureList = chunkMessages.stream()
+            .map(messages -> CompletableFuture.supplyAsync(
+                () -> {
+                    try {
+                        return firebaseMessaging.sendEach(messages);
+                    } catch (FirebaseMessagingException e) {
+                        log.error(e.getMessage());
+                        return null;
+                    }
+                },executor))
+            .map(future -> future.exceptionally(ex -> {
+                log.error(ex.getMessage());
+                return null;
+            }))
+            .toList();
+
+        // 두 개로 나눈 이유는 하나의 파이프라인에서 처리할 시 동기적으로 처리하게 되는 문제가 발생한다.
+        List<BatchResponse> responseList = futureList.stream()
+            .map(CompletableFuture::join)
+            .filter(Objects::nonNull)
+            .toList();
+
     }
 
     public String sendNotification() {
@@ -47,5 +101,55 @@ public class FcmService {
             else log.error("전송 실패");
         }, Executors.newSingleThreadExecutor());
         return "ok";
+    }
+
+    private List<List<Message>> getChunkMessages(Map<Notice, List<RssData>> filterNoticeRssGroup,
+                                                Set<Subscribe> subscribeSet,
+                                                Map<Long, List<Device>> userDeviceGroup) {
+
+        Map<Long, List<RssData>> filterNoticeIdRssGroup = filterNoticeRssGroup.entrySet().stream()
+            .collect(Collectors.toMap(
+                entry -> entry.getKey().getId(),
+                Map.Entry::getValue));
+
+        List<Message> messageList = createMessages(subscribeSet, filterNoticeIdRssGroup, userDeviceGroup);
+
+        return IntStream.iterate(0, i -> i < messageList.size(), i -> i + CHUNK_SIZE)
+            .mapToObj(i -> messageList.subList(i, Math.min(i + CHUNK_SIZE, messageList.size())))
+            .toList();
+    }
+
+    private List<Message> createMessages(Set<Subscribe> subscribeSet,
+                                        Map<Long, List<RssData>> filterNoticeIdRssGroup,
+                                        Map<Long, List<Device>> userDeviceGroup) {
+
+        return forkJoinPool.submit(() -> subscribeSet.parallelStream()
+            .flatMap(subscribe -> createMessageForSubscribe(subscribe, filterNoticeIdRssGroup, userDeviceGroup))
+            .toList()
+        ).join();
+    }
+
+    private Stream<Message> createMessageForSubscribe(Subscribe subscribe,
+                                                    Map<Long, List<RssData>> filterNoticeIdRssGroup,
+                                                    Map<Long, List<Device>> userDeviceGroup) {
+
+        List<RssData> rssDataList = filterNoticeIdRssGroup.getOrDefault(subscribe.getNotice().getId(), Collections.emptyList());
+        List<Device> devices = userDeviceGroup.getOrDefault(subscribe.getUser().getId(), Collections.emptyList());
+        return rssDataList.stream()
+            .flatMap(rssData -> createMessagesForRssData(rssData, devices).stream());
+    }
+
+    private List<Message> createMessagesForRssData(RssData rssData, List<Device> devices) {
+
+        Notification notification = Notification.builder()
+            .setTitle(rssData.title())
+            .build();
+        return devices.stream()
+            .map(device -> Message.builder()
+                .setNotification(notification)
+                .setToken(device.getFcmToken())
+                .putData("click_action", "NOTIFICATION_CLICK")
+                .build())
+            .toList();
     }
 }
