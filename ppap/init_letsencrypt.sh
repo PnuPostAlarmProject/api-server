60df8bae02c9b2af5d45857d59c122c7580192be
Taeho <rjsdnxogh55@gmail.com>
2024-02-04 19:10:45 +0900

    Fix: Remove Docker Download Check

--- ppap/init_letsencrypt.sh
+++ ppap/init_letsencrypt.sh
@@ -2,11 +2,6 @@
 
 export PATH=$PATH:/usr/bin/docker
 
-if ! [ -x "$(command -v docker compose)" ]; then
-  echo 'Error: docker compose is not installed.' >&2
-  exit 1
-fi
-
 domains=(api.pnunotification.site)
 rsa_key_size=4096
 data_path="./certbot"
