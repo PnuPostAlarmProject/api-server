6e3586cc128707a5845ddc7f3cdbe085675aef7c
Taeho <rjsdnxogh55@gmail.com>
2024-02-04 17:01:08 +0900

    Fix: Change ClassPathResource To FileSystemResource

--- ppap/src/main/java/com/ppap/ppap/_core/config/FcmConfig.java
+++ ppap/src/main/java/com/ppap/ppap/_core/config/FcmConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,8 +21,7 @@ public class FcmConfig {
     @Bean
     public FirebaseMessaging initializeFirebaseApp() throws IOException {
 
-        ClassPathResource resource = new ClassPathResource("ppap_fcmkey.json");
-
+        FileSystemResource resource = new FileSystemResource("./src/main/resources/ppap_fcmkey.json");
         InputStream inputStream = resource.getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
