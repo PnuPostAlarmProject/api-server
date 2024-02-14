ead5477a318f952eded95fd315ee0f48c7b4d485
Taeho <rjsdnxogh55@gmail.com>
2024-02-14 21:03:18 +0900

    Refac: Refactoring Send FCM
    - FCM을 Batch로 보내는 방식으로 리팩토링을 진행했습니다.

--- ppap/src/main/java/com/ppap/ppap/_core/batch/DeviceWriter.java
+++ ppap/src/main/java/com/ppap/ppap/_core/batch/DeviceWriter.java
@@ -6,13 +6,14 @@ import java.util.List;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 
+import com.google.firebase.messaging.BatchResponse;
 import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.SendResponse;
 import com.ppap.ppap._core.firebase.FcmTokenValidator;
 import com.ppap.ppap._core.utils.EmailService;
 import com.ppap.ppap.domain.user.entity.Device;
 import com.ppap.ppap.domain.user.repository.DeviceJpaRepository;
 
-import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -32,26 +33,26 @@ public class DeviceWriter implements ItemWriter<Device> {
 	@Override
 	public void write(Chunk<? extends Device> chunk) throws Exception {
 		List<Device> removeDevices = new ArrayList<>();
-		chunk.forEach( device -> {
-			try {
-				fcmTokenValidator.validateTokenThrowException(device.getFcmToken());
-			} catch (FirebaseMessagingException e) {
-				switch (e.getMessagingErrorCode()) {
-					case UNREGISTERED, INVALID_ARGUMENT -> removeDevices.add(device);
-					case QUOTA_EXCEEDED, THIRD_PARTY_AUTH_ERROR, SENDER_ID_MISMATCH -> {
-						try {
-							emailService.sendEmailToAdmin(e.getMessagingErrorCode().toString());
-						} catch (MessagingException ex) {
-							log.error("Device Id: {}, Error Code: {}",
-								device.getId(),
-								e.getMessagingErrorCode().toString()
-							);
-						}
+
+		try {
+			BatchResponse batchResponse = fcmTokenValidator.validateTokenThrowException(
+				chunk.getItems().stream()
+					.map(Device::getFcmToken)
+					.toList());
+
+			List<SendResponse> responseList = batchResponse.getResponses();
+			for (int i=0; i<responseList.size(); i++) {
+				if ( !responseList.get(i).isSuccessful() ) {
+					switch (responseList.get(i).getException().getMessagingErrorCode()) {
+						case UNREGISTERED, INVALID_ARGUMENT, SENDER_ID_MISMATCH ->
+							removeDevices.add(chunk.getItems().get(i));
 					}
 				}
-
 			}
-		});
+		} catch (FirebaseMessagingException e) {
+			emailService.sendEmailToAdmin(e.getMessagingErrorCode().toString());
+			log.error(e.getMessagingErrorCode().toString());
+		}
 
 		if (!removeDevices.isEmpty()) {
 			deviceJpaRepository.deleteAllByIdInBatch(removeDevices.stream()
