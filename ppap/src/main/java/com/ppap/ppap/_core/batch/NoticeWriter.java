9cb8246a35faa95fd12bbe0ecae276cf04f67f9f
Taeho <rjsdnxogh55@gmail.com>
2024-03-26 23:21:31 +0900

    HotFix: HotFix Notice Batch Program Null Error
    - Batch프로그램에 Null Pointer Exception문제가 발생해 이를 수정했습니다.

--- ppap/src/main/java/com/ppap/ppap/_core/batch/NoticeWriter.java
+++ ppap/src/main/java/com/ppap/ppap/_core/batch/NoticeWriter.java
@@ -130,7 +130,7 @@ public class NoticeWriter implements ItemWriter<NoticeDto> {
 
 	private boolean syncNoticeData(Notice notice, NoticeDto noticeDto, Map<String, Univ> univIdMap) {
 		String collegeDepartment = String.format("%s_%s", noticeDto.college(), noticeDto.department());
-		if (!notice.getUniv().equals(univIdMap.get(collegeDepartment))){
+		if (notice.getUniv() == null || !notice.getUniv().equals(univIdMap.get(collegeDepartment))){
 			notice.changeUniv(univIdMap.get(collegeDepartment));
 			return true;
 		}
